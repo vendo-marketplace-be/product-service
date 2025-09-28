f9dea8ae395b69a1df85ace33f1b4965f55bf741
HaaaamBurger <gemasclashes@gmail.com>
2025-09-28 12:21:28 +0300

    #11: init product security

---
+++ src/main/java/com/vendo/product_service/common/config/MapStructConfig.java
@@ -0,0 +1,10 @@
+package com.vendo.product_service.common.config;
+
+import org.mapstruct.MapperConfig;
+import org.mapstruct.ReportingPolicy;
+
+@MapperConfig(
+        componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.ERROR
+)
+public class MapStructConfig { }
